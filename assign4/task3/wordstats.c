f9ce1ee45e2bb74f5c0fa10ac8b7c69ba8d0b8c8
pistullijoanne <41384505+Joannepistulli@users.noreply.github.com>
2022-03-05 03:37:16 +0000

    Bugs

--- assign4/task3/wordstats.c
+++ assign4/task3/wordstats.c
@@ -4,35 +4,38 @@
 
 int main () {
 
-  /* Zero out the array */
   int letter_frequency[26] = {0};
-  int len, i;
+  int len = 0, i, ch;
   char buf[MAX_BUF];
-  char tmp[MAX_BUF];
-  do{
-    strcat(buf, fgets(tmp, MAX_BUF, stdin));
-    //printf("%s\n", tmp);
-  }while(tmp[0]!='\n');
+
+  while((ch = getchar()) != EOF && len < MAX_BUF){
+    
+    buf[len] = ch;
+    len++;
+    
+  }
   
-  len = strlen(buf);
-  i=0;
   do {
-    if(buf[i]>= 'A' && buf[i] <= 'Z'){
-      int j= buf[i]-65;
-      letter_frequency[j]++;
+    
+    if (buf[len] >= 65 && buf[len] <= 90) {
+      
+      letter_frequency[buf[len] - 65] ++;
+      
     }
-    else if (buf[i]>= 'a' && buf[i] <= 'z'){
-      int j= buf[i]-97;
-      letter_frequency[j]++;
+    else if (buf[len] >= 97 && buf[len] <= 122) {
+      letter_frequency[buf[len]-97] ++;
+      
     }
+    
     len--;
-    i++;
-  } while (len > 1);
+    
+  } 
+  while (len > -1);
 
   printf("Distribution of letters in corpus:\n");
+  
   for (i = 0; i < 26; i++) {
-    // 'A' is ASCII code 65
-    printf("%c: %d\n", 65+i, letter_frequency[i]);
+    printf("%c: %d\n", 65+i,   letter_frequency[i]);
   }
-  return 0;
+
 }
