49647a20bd202f7cfb6b0caa7c45a71e2cd3e78d
pistullijoanne <41384505+Joannepistulli@users.noreply.github.com>
2022-03-05 04:37:31 +0000

    Catch Up

--- assign3/task3/mem_bug3.c
+++ assign3/task3/mem_bug3.c
@@ -1,32 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-int * makearray(int size,int base){
 
-  int array[size];
-  int j;
+void set_bits(unsigned * x, unsigned start, unsigned end, unsigned *v) {
 
-  for(j=0;j<size;j++)
-    array[j] = base*=2; //doubling base
-
-  return array;
+  for(int i = end; i <= start; i--){
+     if(v[i] == 0){
+       x[i]=0;
+     } else{ 
+       x[i]=1;
+     }
+  }
+    
 }
 
-int main(){
-  int * a1 = makearray(5,2);
-  int * a2 = makearray(10,3);
-  int j, sum=0;
-
-  for(j=0;j<5;j++){
-    printf("%d ",a1[j]);
-    sum+=a1[j];
-  }
-  printf("\n");
-
-  for(j=0;j<10;j++){
-    printf("%d ",a2[j]);
-    sum+=a2[j];
-  }
-  printf("\n");
-
-  printf("SUM: %d\n", sum);
-}
