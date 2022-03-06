4c0c3826c543dfdc51a862addd9655a096a13aa8
pistullijoanne <41384505+Joannepistulli@users.noreply.github.com>
2022-03-06 18:36:59 +0000

    Tasks:FIXED

--- assign4/task4/bubble.c
+++ assign4/task4/bubble.c
@@ -1,81 +1,58 @@
-/* Example: bubble sort strings in array */
-
-#include <stdio.h>  /* Need for standard I/O functions */
-#include <string.h> /* Need for strlen() */
-#include <stdlib.h>
-#include <stdbool.h>
+#include <stdio.h> 
+#include <string.h> 
 
 
+#define NUM 30 /* number of strings */
+#define LEN 1200 /* max length of each string */
+int main()
 
-#define NUM 30   /* number of strings */
-#define LEN 1200  /* max length of each string */
+{
 
-bool compareStr(char a[], char b[]){
-  int a_len = strlen(a) - 1, b_len = strlen(b) - 1;
-  if(a_len > b_len){
-    for(int i = 0; i < b_len; i++){
-      if(a[i] > b[i]){
-        return true;
-      } else {
-        return false;
-      }
-    }
-  } else {
-    for(int i = 0; i < a_len; i++){
-      if(a[i] > b[i]){
-        return true;
-      } else {
-        return false;
-      }
-    }
-  }
-  return false;
+char Strings[NUM][LEN];
+
+printf("Please enter %d strings, one per line:\n", NUM);
+
+
+
+int i=0;
+
+for(i=0; i<NUM; i++){
+
+    fgets(Strings[i],LEN,stdin);
+
 }
 
-int main()
-{
-  char * Strings[NUM];
-  char buffer[LEN];
+puts("\nHere are the strings in the order you entered:");
 
-  printf("Please enter %d strings, one per line:\n", NUM);
 
-  int stringsize = 0;
-  for(int i = 0; i < NUM; i++){
-    if(fgets(buffer, LEN, stdin) != NULL){
-      stringsize = strlen(buffer);
-      Strings[i] = (char *)malloc(stringsize+1);
-      strcpy(Strings[i], buffer);
+for(i=0; i<NUM; i++){
+
+      printf("[%d] %s\n",i+1,Strings[i]);
+
+}
+
+int j;
+
+char temp[LEN];
+
+for(i=0; i<NUM;i++){
+
+          for(j=0;j<NUM-i-1;j++){
+
+                                if(strcmp(Strings[j],Strings[j+1])>0){                                          strcpy(temp,Strings[j]);                                         strcpy(Strings[j],Strings[j+1]);                                 strcpy(Strings[j+1],temp);
+
+          }
+
     }
-  }
 
-  puts("\nHere are the strings in the order you entered:");
+}
 
-  for(int i = 0; i < NUM; i++){
-    puts(Strings[i]);
-  }
+puts("\nIn alphabetical order, the strings are:");  
 
-  bool sorted = false;
-  int ceiling = NUM - 1;
-    
-  while(!sorted){
-    sorted = true;
-    int i;
-    for(i = 0; i < NUM - 1; i++){
-      if(compareStr(Strings[i], Strings[i+1]) == true){
-        char * temp = Strings[i];
-        Strings[i] = Strings[i+1];
-        Strings[i+1] = temp;
-        sorted = false;
-      }
-    }
-    ceiling--;
-  }
-  
-  puts("\nIn alphabetical order, the strings are:");     
 
-  for(int i = 0; i < NUM; i++){
-    puts(Strings[i]);
-  }
+for(i=0; i<NUM; i++){
+
+                printf("[%d] %s\n",i+1,Strings[i]);
 
 }
 
--- task5/construct_3.c
+++ task5/construct_3.c
@@ -8,24 +8,23 @@ typedef struct node {
 node_t * construct_3() {
     //Allocate three pointers: 
     //x for the first Node, and temporary pointers y and z for the other two Nodes.
-
-     node_t * x=( node_t *)malloc(sizeof(node_t));//x
-     node_t * y=( node_t *)malloc(sizeof(node_t));
-     node_t * z=( node_t *)malloc(sizeof(node_t));
-     x->value=1;
-     y->value=2;
-     z->value=3;
-     x->next=y;
-     y->next=z;
-     z->next=x;
-     return x;
+    node_t* x;
+    node_t* y;
+    node_t* z;
     //Allocate three Node pointees and store references to them in the three pointers.
-
+   x = (node_t*)malloc(sizeof(node_t));
+    y = (node_t*)malloc(sizeof(node_t));
+    z = (node_t*)malloc(sizeof(node_t));
     //Dereference each pointer to store the appropriate number into the value field in its pointee.
-
+    x->value = 1;
+    y->value = 2;
+    z->value = 3;
     //Dereference each pointer to access the .next field in its pointee, 
     //and use pointer assignment to set the .next field to point to the appropriate Node. 
-
+    x->next= y;
+    y->next= z;
+    z->next= x;
+    return x;
 }
 
 //You can ignore the following code for testing
@@ -33,14 +32,7 @@ int dump_all(node_t*);
 int main (int argc, char ** argv) {
 
     node_t * x = construct_3();
-    node_t * y = x->next;
-    node_t * z = y->next;
-    
-    dump_all(x);
-    free(z);
-    free(y);
-    free(x);
-    return 0;
+    return dump_all(x);
 
 }
 
@@ -48,8 +40,8 @@ int dump_all(node_t * x) {
     printf("x -> %d\n", x->value);
     node_t * y = x->next;
     printf("%d -> %d\n", x->value, y->value);
-    node_t * z = y->next;//b.next
-    printf("%d -> %d\n", y->value, z->value);//
+    node_t * z = y->next;
+    printf("%d -> %d\n", y->value, z->value);
     if(z->next != x) {
     	printf("failed");
 	return -1;
