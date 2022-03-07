5fec116bf120f0fa06d54cc36e42cd97ee92f1f5
pistullijoanne <41384505+Joannepistulli@users.noreply.github.com>
2022-03-07 02:09:18 +0000

    assign3 task6

--- assign3/task6/flip_bits.c
+++ assign3/task6/flip_bits.c
@@ -1,23 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Note, the bits are counted from right to left.
+// Note, the bits are counted from right to left. 
 // Flip the bits of x within range [start, end], in which both are inclusive.
 // Assume 0 <= start & end <= 31
-void flip_bits(unsigned *x,
-               unsigned start,
-               unsigned end)
-{
-    unsigned *a = (unsigned *)malloc(sizeof(unsigned) * (end - start + 1));
-
-    for (unsigned i = 0; i <= end; i++)
-    {
-        // flip bit
-        a[i] = ~*x;
-    }
-    printf("array:\n");
-    for (int i = 0; i <= end; i++)
-    {
-        printf("arr[%u] = 0x%x\n\n", i, a[i]);
-    }
+void flip_bits(unsigned * x, unsigned start, unsigned end) {
+  int range = end - start;
+  for(int i = 0; i <= range; i++){
+    *x ^= (1 << (start + i));
+  }
 }
