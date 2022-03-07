d55c2d5ceebbd126fee2a977341874437a0d16a6
pistullijoanne <41384505+Joannepistulli@users.noreply.github.com>
2022-03-07 02:28:56 +0000

    assign 3 - all tasks

--- assign3/task5/set_bits.c
+++ assign3/task5/set_bits.c
@@ -3,30 +3,23 @@
 
 // Set the bits of x within range of [start, end], in which both are inclusive
 // Assume 0 <= start & end <= 31
-void set_bits(unsigned *x,
-              unsigned start,
-              unsigned end,
-              unsigned *v)
-{
-    // array of at least (end-start+1) unsigned integers.
-    unsigned *a = (unsigned *)malloc(sizeof(unsigned) * (end - start + 1));
+void set_bits(unsigned * x,
+             unsigned start,
+             unsigned end,
+             unsigned *y) {
 
-    // v points to an array of at least (end-start+1) unsigned integers
-    v = a;
 
-    for (int i = start; i <= end; i++)
-    {
-        // if v[i] == 0
-        if (v[i] == 0)
-        {
-            // set ( i + start)-th bit of x zero
-            *x ^= i + start;
-        }
-        // otherwise
-        else
-        {
-            // set(i + start) - th bit of x one
-            *x ^= ~(i + start);
-        }
+    int len = end - start + 1;
+    for(int i = 0; i < len; ++i){
+      if(y[i] == 1){
+        *x = *x | (1 << (start + i));
+      }
+      else{
+        *x = *x & ( ~ (1 << (start + i)));
+      }
     }
+    // YOUR CODE HERE
+    // No return value
+    // v points to an array of at least (end-start+1) unsigned integers.
+    // if v[i] == 0, then set (i+start)-th bit of x zero, otherwise, set (i+start)-th bit of x one.
 }
